e11505d3af91d0b2490473452c4b647bca2eb2f8
Taufiq <htaufiq570@gmail.com>
2022-01-16 14:03:01 +0700

    login

---
+++ application/models/Auth_model.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function doLogin()
+    {
+        $email      = $this->input->post('email', true);
+        $pass       = $this->input->post('password', true);
+        $user       = $this->db->where('email', $email)->get('user')->row();
+
+        if ($user) {
+            $isPasswordTrue = password_verify($pass, $user->password);
+            if (!$isPasswordTrue) {
+                return $data["result"] = "wrong_password";
+            } else {
+                if ($user->is_active == 1) {
+                    $this->_add_to_session($user);
+                    if ($user->level == 1) {
+                        return $data["result"] = "admin";
+                    } elseif ($user->level == 2) {
+                        return $data["result"] = "user";
+                    }
+                } else {
+                    return $data["result"] = "not_active";
+                }
+            }
+        }
+        return $data["result"] = "false";
+    }
+
+    public function _add_to_session($row)
+    {
+        $sess = array(
+            'user_logged'   => true,
+            'id'            => $row->id,
+            'name'          => $row->user_name,
+            'level'         => $row->level,
+        );
+        $this->session->set_userdata($sess);
+    }
+
+    public function doLogout()
+    {
+        return $this->session->sess_destroy();
+    }
+}

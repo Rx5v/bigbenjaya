b7518992c3c79abb21d4d48de3be3a756095b611
Taufiq <htaufiq570@gmail.com>
2022-01-24 06:19:13 +0700

    dashboard

--- application/views/admin/include/sidebar.php
+++ application/views/admin/include/sidebar.php
@@ -35,12 +35,12 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                <!-- <li class="nav-item">
-                    <a href="<?= base_url('admin') ?>" class="nav-link active">
+                <li class="nav-item">
+                    <a id="sb-dashboard" href="<?= base_url('admin') ?>" class="nav-link">
                         <i class="fas fa-home nav-icon"></i>
                         <p>Dashboard</p>
                     </a>
-                </li> -->
+                </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-car"></i>
